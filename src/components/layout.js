274e2521abfc179f2d6c084c4176078012bf766b
Patrix2001 <juanpatrick2001@gmail.com>
2022-10-29 10:58:51 +0700

    responsive design

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
     return (
         <div className='min-h-screen'>
             <Navbar />
-            <main className='top-20 relative p-5'>
+            <main className='top-20 relative p-5 max-w-7xl flex flex-col mx-auto'>
                 {children}
             </main>
         </div>
